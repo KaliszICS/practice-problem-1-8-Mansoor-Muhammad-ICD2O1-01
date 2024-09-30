8b832327cd072fe513a6c2e04c85cf70cfe9208d
Mansoor-Muhammad-ICD2O1-01 <mansoor.muhammad@student.tdsb.on.ca>
2024-09-30 13:05:29 +0000

    Answer

--- PP1_8.py
+++ PP1_8.py
@@ -1,24 +1,44 @@
-
+'''
+Lesson: Boolean Logic 
+Author: Mansoor Muhammad 
+Date Created: sept 26, 2024
+'''
 def q1():
-  #Write Assignment code here
+  bool1 = True
+  bool2 = False
+  print(bool1 and bool2)
+
 
 def q2():
-  #Write Assignment code here
+  weed = input("Enter an integer: ")
+  weed = int(weed)
+  print(weed != 0)
+
+   
+
+
+
 
 def q3():
-  #Write Assignment code here
+  education = input("Enter a number: ")
+  print(education >= 0 and education <= 10)
+  
 
 def q4():
-  #Write Assignment code here
+  food = input("Input food: ")
+  drink = input("Input drink: ")
+  print(not food == "pizza" or not drink == "pop")
 
 def q5():
-  #Write Assignment code here
+  integer = input("Enter an integer: ")
+  print(f"The integer {integer} is {integer % 2 == 0}")
 
 #Do not edit code after this
 #Comment out the followwing code when running tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
